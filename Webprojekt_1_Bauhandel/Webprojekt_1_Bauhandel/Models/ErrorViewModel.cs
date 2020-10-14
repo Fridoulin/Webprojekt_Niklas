6e7c3c0d32f85f5a64353d801ada8207bf975c0b
creep2002 <niklasschneider3007@gmail.com>
2020-10-14 16:46:16 +0200

    Projekt erzeugt

---
+++ Webprojekt_1_Bauhandel/Webprojekt_1_Bauhandel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webprojekt_1_Bauhandel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
